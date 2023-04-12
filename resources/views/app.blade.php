34bc5bf56fa464c4b3ca254eafa8dcc6ed75e6d0
alikhan9 <alikhan.zaipoulaiev@outlook.fr>
2023-04-12 02:09:22 +0200

    An other one

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,9 @@
 
     <!-- Scripts -->
     @routes
-    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    {{-- @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"]) --}}
+    <script src="/public/build/assets/app-1eddbf7e.js"></script>
+    <link rel="stylesheet" href="/public/build/assets/app-bd1022d3.css">
     @inertiaHead
 </head>
 
