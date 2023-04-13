2b7d87a2941cd2ceb6bc29d6cd0fbbcb294f0edb
alikhan9 <alikhan.zaipoulaiev@outlook.fr>
2023-04-13 14:50:51 +0200

    Http to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
     }
 }
